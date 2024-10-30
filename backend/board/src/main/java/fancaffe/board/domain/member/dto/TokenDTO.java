feeaf9fa421882516ab651bdbe33d6409f092d6d
hamster0410 <gkstjsdn0011@naver.com>
2024-10-30 16:40:56 +0900

    feat : jwt 토큰 & refresh 토큰 구현

---
+++ backend/board/src/main/java/fancaffe/board/domain/member/dto/TokenDTO.java
@@ -0,0 +1,15 @@
+package fancaffe.board.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDTO {
+    private String AccessToken;
+    private String RefreshToken;
+}
